b8bf0a58fd612883276055f0aebd0299f62e5e02
johannkropf <johann.kropf@gmx.at>
2020-11-13 18:20:52 +0100

    test

--- kilo.c
+++ kilo.c
@@ -459,6 +459,9 @@ void editorProcessKeypress() {
   int c = editorReadKey();
 
   switch (c) {
+    case '\r':
+      /* TODO */
+      break;
     case CTRL_KEY('q'):
       write(STDOUT_FILENO, "\x1b[2J", 4);
       write(STDOUT_FILENO, "\x1b[H", 3);
@@ -470,6 +473,11 @@ void editorProcessKeypress() {
       break;
     case END_KEY:
       if (E.cy < E.numrows) E.cx = E.row[E.cy].size;
+      break;
+    case BACKSPACE:
+    case CTRL_KEY('h'):
+    case DEL_KEY:
+      /* TODO */
       break;
     case PAGE_UP:
     case PAGE_DOWN: {
@@ -488,6 +496,10 @@ void editorProcessKeypress() {
     case ARROW_LEFT:
     case ARROW_RIGHT:
       editorMoveCursor(c);
+      break;
+
+    case CTRL_KEY('l'):
+    case '\x1b':
       break;
 
     default:
